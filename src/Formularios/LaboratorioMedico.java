a4f39b5c055739f5c2079a78e20b2785e581a804
Jose Hernandez <123415299+jrhdezesp@users.noreply.github.com>
2024-11-16 23:48:12 -0600

    Formularios Modificaciones

--- src/Formularios/LaboratorioMedico.java
+++ src/Formularios/LaboratorioMedico.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Clases;
+package Formularios;
 
 /**
  *
@@ -14,7 +14,8 @@ public class LaboratorioMedico {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Login frmLogin = new Login();
+        frmLogin.show();
     }
     
 }
