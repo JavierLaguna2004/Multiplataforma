092a3c657720f487876db9853387e71556d53f9d
PC-Victoria <PC-Victoria@DESKTOP-LDCP4B6>
2024-12-01 20:35:51 -0600

    mi primer commit

---
+++ src/Formularios/mantenimientoPuestos.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Formularios;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement; 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement; 
+import javax.swing.JOptionPane;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSetMetaData;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.ComboBoxModel;
+import javax.swing.JTable;
+import javax.swing.DefaultComboBoxModel;
+
+/**
+ *
+ * @author PC-Victoria
+ */
+public class mantenimientoPuestos {
+     ConexionSQL c=new ConexionSQL();
+    int filas;
+    int codigo;
+
+}
